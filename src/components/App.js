cca3ecd8ce7b30ce834046981ca6d904d2a236de
richard gichuki <chadgichuki@gmail.com>
2022-07-26 13:54:38 +0300

    Polls fetched in App component with a useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar"
 import Home from "./Home"
 
 function App() {
+
+  // State for polls will live in the App due to sharing of data with results and newpoll form
+  // polls & Setpolls passed down as prop to survey
+  const [polls, setPolls] = useState([])
+
+
   return (
     <div className="App">
       {/* <Navbar/> */}
       <Routes>
         <Route path="/" element={<Home/>}/>
-        {/* <Route path="/survey" element={<Survey/>}/>
-        <Route path="/results" element={<Results/>}/>
+        <Route path="/survey" element={<Survey handleSetPolls={setPolls} polls={polls}/>}/>
+        {/* <Route path="/results" element={<Results/>}/>
         <Route path="/contribute" element={<NewPoll/>}/> */}
       </Routes>
     </div>
--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Poll from "./Poll"
 
-function Survey(){
+function Survey({handleSetPolls, polls}){
+
+    // On loading of the Survey component, get the polls to display on Survey page
+    useEffect(() => {
+        fetch("http://localhost:3000")
+        .then(res => res.json())
+        .then(data => handleSetPolls(data.questions))
+    }, [])
+
     return(
         <div className="survey">
             <h1>Vote once for your preferred option on each question</h1>
             {polls.map(poll => {
-                <Poll/>
+                <Poll key={poll.id} id={poll.id} question={poll.question} option1={poll.option1} option2={poll.option2}/>
             })}
         </div>
     )
