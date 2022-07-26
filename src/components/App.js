1952a68918564657cac9abe759bccda93cee92ce
richard gichuki <chadgichuki@gmail.com>
2022-07-26 17:35:33 +0300

    Results table displays table correctly

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar"
 import Home from "./Home"
 import Survey from "./Survey";
+import Results from "./Results";
 
 function App() {
 
   // State for polls will live in the App due to sharing of data with results and newpoll form
   // polls & Setpolls passed down as prop to survey
-  const [polls, setPolls] = useState([])
+  const [polls, setPolls] = useState([
+    {
+      "id": 1,
+      "question": "Which of these 2 cities would you prefer to live in?",
+      "option1": {"image": "", "label": "Nairobi", "votes": 0 },
+      "option2": {"image": "", "label": "Kigali", "votes": 0 }
+    }
+  ])
 
 
   return (
@@ -17,8 +25,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/survey" element={<Survey handleSetPolls={(questions) => setPolls(questions)} polls={polls}/>}/>
-        {/* <Route path="/results" element={<Results/>}/>
-        <Route path="/contribute" element={<NewPoll/>}/> */}
+        <Route path="/results" element={<Results polls={polls}/>}/>
+        {/* <Route path="/contribute" element={<NewPoll/>}/> */}
       </Routes>
     </div>
   );
---
+++ src/components/Results.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Results({polls}){
+    return(
+        <table>
+            <thead>
+                <tr>
+                <th>ITEM</th>
+                <th>QUESTION</th>
+                <th>OPTION 1</th>
+                <th>VOTES</th>
+                <th>OPTION 2</th>
+                <th>VOTES</th>
+                </tr>
+            </thead>
+            <tbody>
+                {polls.map(poll => {
+                    return(
+                        <tr key={poll.id}>
+                            <td>{poll.id}</td>
+                            <td>{poll.question}</td>
+                            <td>{poll.option1.label}</td>
+                            <td>{poll.option1.votes} Votes</td>
+                            <td>{poll.option2.label}</td>
+                            <td>{poll.option2.votes} Votes</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
+
+export default Results
