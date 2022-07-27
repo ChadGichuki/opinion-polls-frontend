cae435da911edb637b4ca6b01638377cc5f67760
richard gichuki <chadgichuki@gmail.com>
2022-07-27 16:48:43 +0300

    Added bootstrap styling

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -35,32 +35,57 @@ function Poll({id, question, option1, option2}){
     }
 
     return(
-        <div className="card" style={{"width": "65rem"}}>
+        <div id="polls">
             <h2>{question}</h2>
-            <div >
-                <div className="options">
-                    <img src={option1.image} alt={option1.label} className="card-img-top"/>
-                    <p className="card-title">{option1.label}</p>
-                    <button className="btn btn-primary" onClick={handleOption1Click}>Vote</button>
+            <div className="pollsflex">
+                <div className="card mb-3" style={{"max-width": "65rem"}}>
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={option1.image} alt={option1.label} className="img-fluid rounded-start" />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <p className="card-title">{option1.label}</p>
+                                <button className="btn btn-primary" onClick={handleOption1Click}>Vote</button>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
-                <div className="options">
-                    <img src={option2.image} alt={option2.label} className="card-img-top"/>
-                    <p className="card-title">{option2.label}</p>
-                    <button className="btn btn-primary" onClick={handleOption2Click}>Vote</button>
+                <h3>Vs</h3>
+                <div className="card mb-3" style={{"width": "65rem"}}>
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={option2.image} alt={option2.label} className="img-fluid rounded-start" />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <p className="card-title">{option2.label}</p>
+                                <button className="btn btn-primary" onClick={handleOption2Click}>Vote</button>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div>        
     )
 }
 
 export default Poll
 
 
-{/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+{/* <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
   </div>
 </div> */}
